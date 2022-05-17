55127115fa7ccf78f006a3efab5dd12360da0d85
bcqsd <20204248@cqu.edu.cn>
2022-05-17 15:47:48 +0800

    todo:change background to global css

---
+++ components/Picker/Picker.vue
@@ -0,0 +1,35 @@
+<template>
+	
+</template>
+
+<script setup>
+	const props=defineProps({
+		range:Array
+	})
+   
+</script>
+
+<style scoped>
+	@font-face {
+		font-family:Semibold ;
+		src: url('../../static/fonts/Semibold.ttf');
+	}
+	.top{
+		position: relative;
+		font-family: Semibold;
+		/* 单行显示 */
+		white-space: nowrap; 
+		font-weight: bold;
+		overflow: hidden;
+		color:brown;
+	}
+	#top-image{
+		position: absolute;
+	}
+	#top-word{
+		display: flex;
+		height: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
--- pages/index/config.vue
+++ pages/index/config.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="mirror-top">
+	<view class="mirror-top" :style="style">
 		<navigator url="/pages/index/index">
 			<image id="mirror-top-image1" src="/static/buttons/return.png" alt=""/>
 		</navigator>
@@ -7,15 +7,39 @@
 			<image id="mirror-top-image2" src="/static/buttons/exit.png" alt=""/>
 		</navigator>
 		<view class="mirror-top-main">
-		     <view class="mirror-top-main-title">
-		     	主题修改
+		     <view class="mirror-top-main-item">
+				 <view>主题选择</view>
+				<scroll-view @scroll="chooseTheme($event)" scroll-y="true" class="item-scroll">
+					<view v-for="item in themes">{{item}}</view>
+				</scroll-view>
 		     </view>
 		</view>
 	</view>
 </template>
 
 <script setup>
-	
+	import {ref} from 'vue'
+	const themes=['卷轴','红木','木质','羊皮纸']
+	const themeMap=[
+	'/static/textures/relumeBack.jpg',
+	'/static/textures/Wood1.jpg',
+	'/static/textures/Wood2.jpg',
+	'/static/textures/yangpi.jpg',
+	]
+	const style=ref({
+		backgroundImage:`url(${themeMap[0]})`
+	})
+	function chooseTheme(e){
+         let scrollTop=e.detail.scrollTop
+	     let ind=Math.floor((scrollTop)/20);
+		 let url=themeMap[ind]
+		 let prevUrl=localStorage.getItem('themeUrl')
+		 if(!prevUrl||prevUrl!==url){
+			 localStorage.setItem('themeUrl',url)
+			 style.value.backgroundImage=`url(${url})`
+			 console.log(style.value.backgroundImage)
+		 }
+	}
 </script>
 
 <style lang="scss" scoped>
@@ -41,7 +65,7 @@
 	 width: 100%;
      height: 100vh;
 	 overflow: hidden;
-	 background-image: url('/static/textures/relumeBack.jpg');
+	 // background-image: url('/static/textures/relumeBack.jpg');
 	 background-size: 100vw 100vh;
 	 font-family: Semibold;
 	 font-size: 0.35rem;
@@ -55,8 +79,17 @@
  	 margin-left: 5vw;
  	 margin-right: 5vw;
  }
- .mirror-top-main-title{
+ .mirror-top-main-item{
 	 font-size: 0.6rem;
+	 display: flex;
+	 justify-content: space-around;
+	 flex-wrap: nowrap;
+	 span{
+		 height: 0.6rem;
+	 }
+	 .item-scroll{
+		 height: 0.6rem;
+		 width: 3rem;
+	 }
  }
- 
 </style>
